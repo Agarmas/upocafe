6b3d22b3aaf78b6928b6f7dafc798ab4b8f09ef2
Antonio Garrido Massé <93100218+Agarmas@users.noreply.github.com>
2023-11-11 23:31:27 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -16,7 +16,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'product', 'point_of_sale', 'mrp'],
+    'depends': ['base', 'product', 'mrp'],
 
     # always loaded
     'data': [
