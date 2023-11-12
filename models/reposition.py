28c9f301d1409328c135aa759e591b41db132689
Amoremend <91496961+Amoremend@users.noreply.github.com>
2023-11-12 12:54:26 +0100

    Delete models/reposition.py

--- models/reposition.py
+++
@@ -1,7 +0,0 @@
-from odoo import models, fields, api
-
-class Reposition(models.Model):
-     _inherit = 'purchase.order'
-
-     provider_id = fields.Many2one("upocafe.provider",string="Provides")
-     machine_id = fields.Many2one("upocafe.machine", string="Replenishes")
