0de648021601459c4ccb0a2afae5896be0885e02
Marquinyos1 <146761433+Marquinyos1@users.noreply.github.com>
2023-11-12 18:26:38 +0100

    Add files via upload

---
+++ upocafe/models/status.py
@@ -0,0 +1,10 @@
+from odoo import models, fields, api
+
+
+class Status(models.Model):
+     _name = 'upocafe.status'
+     _description = 'Status of a machine'
+
+     status = fields.Char(string="Estado", size=255 ,required=True, help="status of a machine")
+
+     machines_ids = fields.One2many("upocafe.machine", 'status_id',string="Maquinas con este estado")
