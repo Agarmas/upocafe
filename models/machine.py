77a645cdbbe6f8e8482a5f253a2be5603800ed59
Amoremend <91496961+Amoremend@users.noreply.github.com>
2023-12-07 19:27:17 +0100

    Add files via upload

--- models/machine.py
+++ models/machine.py
@@ -5,7 +5,7 @@ class Machine(models.Model):
      _name = 'upocafe.machine'
      _description = 'Coffee vending machines'
 
-     model_name = fields.Char(string="Nombre del modelo", size=255 ,required=True, help="name of the model")
+     name = fields.Char(string="Nombre del modelo", size=255 ,required=True, help="name of the model")
      location = fields.Char('Localizacion', size=255, required=True, help="machines location")
      create_date = fields.Datetime('Fecha del alta',required=True, autodate = True)
      status_id = fields.Many2one("upocafe.status",string="Estado actual")
