404d66165b276be05d762970395f6414603475e2
Antonio Garrido Masse <agarmas03@gmail.com>
2023-12-07 15:38:36 +0100

    Adaptación del cambio de concepto de pago de productos a pago de ordenes de producción

--- models/payment.py
+++ models/payment.py
@@ -4,17 +4,17 @@ class Payment(models.Model):
     _name = 'upocafe.payment'
     _description = 'Pago de un conjunto de productos'
 
-    id = fields.Char(string='Payment ID', required=True)
     date = fields.Date(string='Fecha')
-    amount = fields.Float(string='Cantidad')
+    amount = fields.Float(string='Precio', compute='_compute_amount')
     payment_method_id = fields.Many2one('upocafe.payment_method', string='Metodo de pago')
     partner_id = fields.Many2one('res.partner', string='Cliente')
     currency_id = fields.Many2one('res.currency', string='Moneda')
     cancelation_id = fields.Many2one('upocafe.cancelation', string='Cancelacion')
-    product_ids = fields.Many2many('product.product', string='Productos')
+    production_ids = fields.Many2many('mrp.production', string='Ordenes de producción')
     machine_id = fields.Many2one('upocafe.machine', string='Máquina')
 
-    @api.depends('product_ids')
+    @api.depends('production_ids.product_qty', 'production_ids.product_id.lst_price')
     def _compute_amount(self):
         for record in self:
-            record.amount = sum(product.price_list for product in record.product_ids)
+            print("Computing amount for record:", record.id)
+            record.amount = sum(production.product_qty * production.product_id.lst_price for production in record.production_ids)
