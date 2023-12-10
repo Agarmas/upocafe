829ccbbaa0e8c882fc95ecc06883854cc9e12943
Marquinyos1 <146761433+Marquinyos1@users.noreply.github.com>
2023-12-10 20:24:28 +0100

    Add files via upload

--- models/status.py
+++ models/status.py
@@ -6,5 +6,4 @@ class Status(models.Model):
      _description = 'Status of a machine'
 
      status = fields.Char(string="Estado", size=255 ,required=True, help="status of a machine")
-
-     machines_ids = fields.One2many("upocafe.machine", 'status_id',string="Maquinas con este estado")
+     machines_ids = fields.One2many("upocafe.machine", 'status_id', string="Maquinas con este estado")
