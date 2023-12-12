dfa02e4e51d24fc2d0015cdd90dca58475c756af
Amoremend <91496961+Amoremend@users.noreply.github.com>
2023-12-12 12:26:31 +0100

    Add files via upload

--- models/payment.py
+++ models/payment.py
@@ -13,3 +13,59 @@ class Payment(models.Model):
     cancelation_id = fields.Many2one('upocafe.cancelation', string='Cancelacion')
     product_ids = fields.Many2many('product.product', string='Productos')
     machine_id = fields.Many2one('upocafe.machine', string='Máquinas')
+
+    @api.depends('production_ids.product_qty', 'production_ids.product_id.lst_price')
+    def _compute_amount(self):
+        for record in self:
+            record.amount = sum( (production.product_qty * production.product_id.lst_price) for production in record.production_ids)
+
+    # Validaciones de la  fecha
+    @api.onchange('date')
+    def onchange_date(self):
+        resultado = {}
+        if self.date and not self._is_valid_date_format(self.date):
+            resultado = {
+                'value': {'date': ''},
+                'warning': {
+                    'title': 'Formato de fecha incorrecto',
+                    'message': 'El formato de la fecha no es válido. Utiliza el formato YYYY-MM-DD.'
+                }
+            }
+        return resultado
+
+    def _is_valid_date_format(self, date_string):
+        try:
+            fields.Date.from_string(date_string)
+            return True
+        except ValueError:
+            return False
+
+    # Fin de las validaciones de la fecha
+
+    # Método para "limpiar el carrito" de productos
+    def btn_removeProductions(self):
+        # Borramos los productos de este pago
+        self.write({'production_ids':[(5,)]})
+
+
+        # Borramos las ordenes de producción para no tener ordenes de producción invalidas
+        production_orders = self.env['mrp.production'].search([('id', 'in', self.production_ids.ids)])
+        production_orders.unlink()
+
+        # Recargo la vista para que se vea reflejado
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload',
+        }
+    
+
+    # Método para poner la fecha del pago a hoy
+    def btn_setDate(self):
+        # Fijar el campo date a la fecha de hoy
+        today_date = fields.Date.today()
+        self.write({'date': today_date})
+        
+        return {
+            'type': 'ir.actions.client',
+            'tag': 'reload', 
+        }
