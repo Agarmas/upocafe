f3a55c926a35c57dc251b8e07bf98bc6dca64868
psantro <pablosantron@gmail.com>
2023-12-21 00:10:19 +0100

    Contador de transacciones realizadas
    Añadido total de transacciones realizadas con un método de pago

--- models/payment_method.py
+++ models/payment_method.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 from odoo import models, fields, api
 
 
@@ -7,5 +5,15 @@ class PaymentMethod(models.Model):
      _name = 'upocafe.payment_method'
      _description = 'Method Payment'
 
-     name = fields.Char(string='Nombre', required=True, help='Nombre del método de pago')
-     payment_ids = fields.One2many('upocafe.payment', 'payment_method_id', string='Pagos')
+     name = fields.Char(string='Nombre', required=True,
+                        help='Nombre del método de pago')
+     payment_ids = fields.One2many('upocafe.payment', 'payment_method_id',
+                                   string='Pagos',
+                                   help='Pagos realizados con este método de pago')
+     transactions = fields.Integer(compute='_totalTransactions',
+                                   string='Pagos totales', store=True,
+                                   help='Total de pagos realizafos con este método de pago')
+
+     def _totalTransactions(self):
+          for record in self:
+               record.transactions = len(record.payment_ids)
