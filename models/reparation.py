6db2de16920eb2935c896f0ef9020b4bb49774b7
psantro <pablosantron@gmail.com>
2023-12-20 21:30:20 +0100

    Vistas mejoradas

--- models/reparation.py
+++ models/reparation.py
@@ -7,9 +7,9 @@ class Reparation(models.Model):
      _name = 'upocafe.reparation'
      _description = 'Reparation'
 
-     init_datetime = fields.Datetime(string='Comenzada', required=True, autodate=True)
-     fini_datetime = fields.Datetime(string='Finalizada', required=True)
-     repair_summary = fields.Text(string='Resumen', required=True)
-     repair_description = fields.Text(string='Descripción Técnica', required=True)
-     machine_id = fields.Many2one('upocafe.machine', string='Máquina')
-     employee_ids = fields.Many2many('hr.employee', string='Empleados')
+     machine_id = fields.Many2one('upocafe.machine', string='Máquina', required=True)
+     name = fields.Text(string='Resumen', required=True)
+     description = fields.Text(string='Descripción Técnica')
+     started = fields.Datetime(string='Comenzada', required=True, autodate=True)
+     ended = fields.Datetime(string='Finalizada')
+     employee_ids = fields.Many2many('hr.employee', string='Empleados', required=True)
