23e9ab10b221b061053baa2051b5036b82f38e4b
psantro <pablosantron@gmail.com>
2023-12-21 00:57:59 +0100

    Añadido notebooks
    No valen pa' mucho.
    Pero el cliente (Aurelio mamón) lo exige.

--- models/reparation.py
+++ models/reparation.py
@@ -8,7 +8,7 @@ class Reparation(models.Model):
      machine_id = fields.Many2one('upocafe.machine', string='Máquina',
                                   required=True)
      name = fields.Text(string='Resumen', required=True)
-     description = fields.Text(string='Descripción Técnica')
+     description = fields.Text(string='Descripción técnica')
      started = fields.Datetime(string='Comenzada', autodate=True,
                                required=True)
      ended = fields.Datetime(string='Finalizada')
