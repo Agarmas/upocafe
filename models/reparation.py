480ee1c4e8550be259ad5ae7d51afa16afaa8d58
psantro <pablosantron@gmail.com>
2023-12-21 14:55:34 +0100

    Añadiendo un botoncito
    Botón para finalizar la reparación.

--- models/reparation.py
+++ models/reparation.py
@@ -5,38 +5,25 @@ class Reparation(models.Model):
      _name = 'upocafe.reparation'
      _description = 'Reparation'
 
-     machine_id = fields.Many2one('upocafe.machine', string='Máquina', require=True)
-     name = fields.Text(string='Resumen', required=True)
-     description = fields.Text(string='Descripción técnica')
-     started = fields.Datetime(string='Comenzada', autodate=True, required=True)
-     ended = fields.Datetime(string='Finalizada')
-     employee_ids = fields.Many2many('hr.employee', string='Empleados', required=True)
+     name = fields.Text(required=True, string='Resumen', help='Resumen de la causa de la reparación')
+     description = fields.Text(string='Descripción técnica', help='Descripción de la causa de la reparación para los empleados involucrados')
+     started = fields.Datetime(default=fields.Datetime.now, required=True, string='Comenzada', help='Fecha de inicio de la reparación')
+     ended = fields.Datetime(string='Finalizada', help='Fecha de finalización de la reparación')
+     machine_id = fields.Many2one('upocafe.machine', required=True, string='Máquina', help='Máquina afectada por la reparación')
+     employee_ids = fields.Many2many('hr.employee', required=True, string='Empleados', help='Empleados involucrados')
 
      @api.onchange('started', 'ended')
      def _onchange_started_ended(self):
-          if self.started:
-               if not fields.Date.from_string(self.started):
-                    return {
-                         'value': {'started': ''},
-                         'warning': {
-                              'title': 'Formato de fecha incorrecto',
-                              'message': 'El formato de la fecha no es válido. Utiliza el formato YYYY-MM-DD.'
-                         }
+          if self.started and self.ended and (self.started >= self.ended):
+               return {
+                    'value': {'ended': ''},
+                    'warning': {
+                         'title': '¿Tratas de viajar en el tiempo?',
+                         'message': 'La fecha de finalización no puede ser anterior a la fecha de inicio.'
                     }
-               if not fields.Date.from_string(self.ended):
-                    return {
-                         'value': {'ended': ''},
-                         'warning': {
-                              'title': 'Formato de fecha incorrecto',
-                              'message': 'El formato de la fecha no es válido. Utiliza el formato YYYY-MM-DD.'
-                         }
-                    }
-               if self.started and self.ended and (self.started >= self.ended):
-                    return {
-                         'value': {'ended': ''},
-                         'warning': {
-                              'title': '¿Tratas de viajar en el tiempo?',
-                              'message': 'La fecha de finalización no puede ser anterior a la fecha de inicio.'
-                         }
-                    }
+               }
           return {}
+
+     def btn_end_reparation(self):
+          if not self.ended:
+               self.write({'ended': fields.Datetime.now})
