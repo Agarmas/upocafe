7143c9d8b366f0abcd1e70cc59598313a1b3f40d
Marquinyos1 <146761433+Marquinyos1@users.noreply.github.com>
2023-12-21 14:27:17 +0100

    Add files via upload

--- models/status.py
+++ models/status.py
@@ -5,5 +5,17 @@ class Status(models.Model):
      _name = 'upocafe.status'
      _description = 'Status of a machine'
 
-     status = fields.Char(string="Estado", size=255 ,required=True, help="status of a machine")
+     name = fields.Selection([('activo','Activo'),
+                              ('averiado','Averiado'),
+                              ('en reparacion','En Reparacion'),],
+                              'Estados de una maquina', default='activo')
      machines_ids = fields.One2many("upocafe.machine", 'status_id', string="Maquinas con este estado")
+     
+     def btn_status_to_activo(self):
+          self.write({'name':'activo'})
+
+     def btn_status_to_reparacion(self):
+          self.write({'name':'en reparacion'})
+
+     def btn_status_to_averiado(self):
+          self.write({'name':'averiado'})
